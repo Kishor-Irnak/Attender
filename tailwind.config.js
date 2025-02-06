50c7c251436a48491dc1cbbb0c21e2336cb3adfc
Kishor.Ai <140939437+Kishor-Irnak@users.noreply.github.com>
2025-02-06 18:54:57 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    darkMode: 'class',
+    // ...
+  }
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('flowbite-typography'),
+    // ...
+  ],
+
+plugins: [
+  require('flowbite/plugin')({
+      wysiwyg: true,
+  }),
+  // ... other plugins
+]
